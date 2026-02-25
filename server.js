e20e7676205bdef734ba915e06f7551ea9e1db93
Jabir <jabirsahani@gmail.com>
2026-02-25 16:16:55 +0530

    add health endpoint

--- server.js
+++ server.js
@@ -246,6 +246,10 @@ app.get("/", (req, res) => {
 </html>`);
 });
 
+app.get("/health", (req, res) => {
+    res.send("ok");
+});
+
 app.listen(3000, () => {
     console.log("Server running at http://localhost:3000");
 });
